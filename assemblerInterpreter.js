f789b0c49d358aacb7bfb5cbb7aaa3fae2c59596
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-07-26 23:00:02 -0500

    trimstructions parsed and sanitized, next work on arithmatic

--- assemblerInterpreter.js
+++ assemblerInterpreter.js
@@ -1,39 +1,56 @@
-assemblerInterpreter(program) {
+assemblerInterpreter = (program) => {
     // way harder than last version
     // first we have to parse the program into instructions
+    instructions = program.split("\n")
+    trimstructions = instructions.map(x => x.trim())
+    console.log(program)
+    console.log(instructions)
+    console.log(trimstructions)
+
 
 
     // 
-    let registers = {}
-    let i = 0
-    while (i < program.length) {
-        instruction = program[i]
-        parts = instruction.split(" ")
-        console.log(parts)
-        switch (parts[0]) {
-            case "mov":
-                if (!isNaN((parts[2]) % 1)) {
-                    registers[parts[1]] = parts[2]
-                } else {
-                    registers[parts[1]] = registers[parts[2]]
-                }
-                i++
-                break;
-            case "inc":
-                registers[parts[1]]++
-                i++
-                break;
-            case "dec":
-                registers[parts[1]]--
-                i++
-                break;
-            case "jnz":
-                if (registers[parts[1]] != 0) {
-                    i += parseInt(parts[2])
-                } else { i++ }
-                break;
-        }
-        console.log(i, registers)
-    };
-    return registers
+    // let registers = {}
+    // let i = 0
+    // while (i < program.length) {
+    //     instruction = program[i]
+    //     parts = instruction.split(" ")
+    //     console.log(parts)
+    //     switch (parts[0]) {
+    //         case "mov":
+    //             if (!isNaN((parts[2]) % 1)) {
+    //                 registers[parts[1]] = parts[2]
+    //             } else {
+    //                 registers[parts[1]] = registers[parts[2]]
+    //             }
+    //             i++
+    //             break;
+    //         case "inc":
+    //             registers[parts[1]]++
+    //             i++
+    //             break;
+    //         case "dec":
+    //             registers[parts[1]]--
+    //             i++
+    //             break;
+    //         case "jnz":
+    //             if (registers[parts[1]] != 0) {
+    //                 i += parseInt(parts[2])
+    //             } else { i++ }
+    //             break;
+    //     }
+    //     console.log(i, registers)
+    // };
+    // return registers
 }
+
+assemblerInterpreter(`; My first program
+mov  a, 5
+inc  a
+call function
+msg  '(5+1)/2 = ', a    ; output message
+end
+
+function:
+    div  a, 2
+    ret`)
