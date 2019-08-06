4e3e6d2872412f9be5e4330d24aa41d7524d75dc
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-08-06 15:03:52 -0500

    squared up the strings, not exactly elegant but functional

--- codingWithSquaredStrings.js
+++ codingWithSquaredStrings.js
@@ -1,39 +1,113 @@
+// function code(text) {
+//     let squaretext = text.slice()
+//     let length = text.length
+//     let n = 0//find smallest integer n such that n*n>=length
+//     while (n * n < length) { n++ }
+//     let target = n * n
+//     let filler = String.fromCharCode(11)
+//     while (squaretext.length < target) { squaretext += filler }
+//     let plaintextArray = []
+//     let encodedArray = []
+//     let empty = [""]
+//     for (let i = 0; i < n; i++) { plaintextArray[i] = squaretext.slice(i * n, (i * n) + (n)); empty[i] = "" }
+//     // console.log(plaintextArray)
+//     for (let i = 0; i < n; i++) { plaintextArray[i] = plaintextArray[i].split(""); encodedArray.push(new Array(...empty)) }
+//     let local = ""
+//     for (let i = n - 1; i >= 0; i--) {
+//         for (let j = 0; j < n; j++) {
+//             local = plaintextArray[n - 1 - j][i]
+//             encodedArray[i][j] = local
+//         }
+//     }
+//     // console.log(encodedArray)
+//     for (let i = 0; i < n; i++) { encodedArray[i] = encodedArray[i].join("") }
+//     let answer = encodedArray.join("\n")
+//     return answer
+// }
+
+// function decode(ciphertext) {
+//     let squared = ciphertext.split("\n")
+//     // console.log(squared)
+//     let n = squared[0].length
+//     // console.log(`n=${n}`)
+//     squared.forEach((value, index) => {
+//         squared[index] = value.split("")
+//     })
+//     let decodedArray = []
+//     let empty = []
+//     for (let i = 0; i < n; i++) { empty[i] = "" }
+//     for (let i = 0; i < n; i++) { decodedArray.push(new Array(...empty)) }
+
+//     console.log(squared)
+//     console.log(decodedArray)
+//     let local = ""
+
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < n; j++) {
+//             local = squared[i][n - 1 - j]
+//             console.log(local)
+//             decodedArray[j][i] = local
+//         }
+//     }
+//     // for (let i = 0; i <= n - 2; i++) {
+//     //     for (let j = n; j > 0; j--) {
+//     //         // local = plaintextArray[n - 1 - j][i]
+//     //         // encodedArray[i][j] = local
+//     //         local = squared[n - 1 - j][i]
+//     //         decodedArray[i][j] = local
+//     //     }
+//     // }
+//     for (let i = 0; i < n; i++) { decodedArray[i] = decodedArray[i].join("") }
+//     console.log(decodedArray)
+// }
+
+
+
+console.log(decode(code("ABCDEFGHI")))
+
+
 function code(text) {
     let squaretext = text.slice()
     let length = text.length
-    //     find smallest integer n such that n*n>=length
-    let n = 0
+    let n = 0//find smallest integer n such that n*n>=length
     while (n * n < length) { n++ }
     let target = n * n
     let filler = String.fromCharCode(11)
     while (squaretext.length < target) { squaretext += filler }
-    // good through here
     let plaintextArray = []
     let encodedArray = []
     let empty = [""]
     for (let i = 0; i < n; i++) { plaintextArray[i] = squaretext.slice(i * n, (i * n) + (n)); empty[i] = "" }
-    console.log(plaintextArray)
     for (let i = 0; i < n; i++) { plaintextArray[i] = plaintextArray[i].split(""); encodedArray.push(new Array(...empty)) }
-    //   let practice=[['a','b','c'],['d','e','f'],['g','h','i']]
-    //   let goal=[['','',''],['','',''],['','','']]
     let local = ""
     for (let i = n - 1; i >= 0; i--) {
         for (let j = 0; j < n; j++) {
-            //   for(let i=0; i<3; i++){
-            //     for(let j=0; j<3; j++){
-            //      goal[i][j]=practice[2-j][i]
             local = plaintextArray[n - 1 - j][i]
-            //         console.log(local)
             encodedArray[i][j] = local
         }
     }
-    //     console.log(goal)
-    console.log(encodedArray)
     for (let i = 0; i < n; i++) { encodedArray[i] = encodedArray[i].join("") }
-    let answer = encodedArray.join("/n")
+    let answer = encodedArray.join("\n")
     return answer
 }
 
-function decode(s) {
-    // your code
+function decode(ciphertext) {
+    let squared = ciphertext.split("\n")
+    let n = squared[0].length
+    squared.forEach((value, index) => {
+        squared[index] = value.split("")
+    })
+    let decodedArray = []
+    let empty = []
+    for (let i = 0; i < n; i++) { empty[i] = "" }
+    for (let i = 0; i < n; i++) { decodedArray.push(new Array(...empty)) }
+    let local = ""
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            local = squared[i][n - 1 - j]
+            decodedArray[j][i] = local
+        }
+    }
+    for (let i = 0; i < n; i++) { decodedArray[i] = decodedArray[i].join("") }
+    return decodedArray.join("").trim()
 }
