aa8eb9da1eb5fb65bab1820debc116a1bca14395
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-08-13 21:22:56 -0500

    solved, looping conditions threw me for a loop, thanks for the guy who clarified in the discussions

--- customPaintf**kInterpreter.js
+++ customPaintf**kInterpreter.js
@@ -1,10 +1,8 @@
 function interpreter(code, iterations, width, height) {
-    console.log(iterations, code)
     let memory = []
     let wide = []
     for (let i = 0; i < width; i++) { wide.push(0) }
     for (let i = 0; i < height; i++) { memory.push([...wide]) }
-    console.log(memory)
     let pointer = [0, 0]
     let commands = code.split('')
     let comma = 0//short for which command we are on
@@ -16,28 +14,24 @@ function interpreter(code, iterations, width, height) {
                 if (pointer[0] < 0) { pointer[0] = height - 1 }
                 comma++
                 step++
-
                 break;
             case "s":
                 pointer[0]++
                 if (pointer[0] == height) { pointer[0] = 0 }
                 comma++
                 step++
-
                 break;
             case "e":
                 pointer[1]++
                 if (pointer[1] == width) { pointer[1] = 0 }
                 comma++
                 step++
-
                 break;
             case "w":
                 pointer[1]--
                 if (pointer[1] < 0) { pointer[1] = width - 1 }
                 comma++
                 step++
-
                 break;
             case "*":
                 memory[pointer[0]][pointer[1]] == 0 ? memory[pointer[0]][pointer[1]] = 1 : memory[pointer[0]][pointer[1]] = 0
@@ -49,23 +43,16 @@ function interpreter(code, iterations, width, height) {
                 if (memory[pointer[0]][pointer[1]] == 0) { comma = findCloser(comma, commands) }
                 else { comma++ }
                 step++
-
                 break;
             case "]":
-                if (memory[pointer[0]][pointer[1]] == 1) { comma = findOpener(comma, commands) }
-                else { comma++; step++ }
-                //                 step++
-
+                if (memory[pointer[0]][pointer[1]] == 1) { comma = findOpener(comma, commands); step++ }
+                else { comma++ }
                 break;
             default:
                 comma++
-                //                 step++
                 break;
         }
-        console.log(step, comma, pointer, memory[pointer[0]][pointer[1]])
     }
-    console.log(memory)
-
     let flatten = []
     memory.forEach(array => flatten.push(array.join("")))
     let memoryString = flatten.join("\r\n")
@@ -82,9 +69,7 @@ function findCloser(index, commandArray) {
 function findOpener(index, commandArray) {
     let counter = 0
     for (let j = index - 1; j >= 0; j--) {
-        if (counter == 0 && commandArray[j] == "[") {
-            return j + 1
-        }
+        if (counter == 0 && commandArray[j] == "[") { return j + 1 }
         else if (commandArray[j] == "]") { counter++ }
         else if (commandArray[j] == "[") { counter-- }
     }
