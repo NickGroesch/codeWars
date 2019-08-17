46e0d05c1e551fde859f66723f92c455d7fdf68e
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-08-16 20:53:26 -0500

    clearing up the backlog maxSubarraySum is gtg

---
+++ maximumSubarraySum.js
@@ -0,0 +1,21 @@
+var maxSequence = function (array) {
+    let maximum = 0
+    let temp
+    if (array.length == 0) { return maximum } else {
+        for (let start = 0; start < array.length - 1; start++) {
+            for (let end = start + 1; end < array.length - 1; end++) {
+                let local = array.slice(start, end)
+                let sum = local.reduce((accumulator, currentValue) => accumulator + currentValue)
+                if (sum > maximum) { maximum = sum }
+            }
+        }
+        return maximum
+    }
+
+}
+
+
+
+
+console.log(maxSequence([]))// 0
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))// 6
