e1e004f576e6deb2da18830f26b5ae339b2aacb0
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-08-22 16:03:49 -0500

    worked on cleaning up the data for edges since .includes doesn't evaluate arrays as arguments well

--- directedGraph.js
+++ directedGraph.js
@@ -1,11 +1,48 @@
+// inspired by https://www.geeksforgeeks.org/implementation-graph-javascript/
+// inspired by https://hackernoon.com/the-javascript-developers-guide-to-graphs-and-detecting-cycles-in-them-96f4f619d563
+
 class Graph {
     constructor(noOfVertices) {
         this.noOfVertices = noOfVertices
         this.AdjList = new Map();
     }
-    //addVertex (v),
-    // addEdge(v,w)
-    // printGraph()
-    // bfs(v)
-    // dfs(v)
+    addVertex(v) {
+        this.AdjList.set(v, [])
+    }
+    addDirectedEdge(v, w) {// v= source, w=destingation
+        this.AdjList.get(v).push(w)
+    }
+    addEdge(v, w) {//for undirected graph
+        this.AdjList.get(v).push(w)
+        this.AdjList.get(w).push(v)
+    }
+    printGraph() {
+        let get_vertices = this.AdjList.keys()
+        for (let vertex of get_vertices) {
+            let values = this.AdjList.get(vertex)
+            let adjacencies = ""
+            for (let edge of values) {
+                adjacencies += `${edge} `
+            }
+            console.log(`${vertex} -> ${adjacencies}`)
+        }
+    }
+    // bfs(startingNode)//breadth first search
+    // dfs(startingNode)//depth first search
 }
+
+let testGraph = new Graph(6)
+let testVertices = ['a', 'b', 'c', 'd', 'e', 'f']
+let testEdges = [
+    ['a', 'b'],
+    ['a', 'd'],
+    ['a', 'e'],
+    ['b', 'c'],
+    ['d', 'e'],
+    ['e', 'f'],
+    ['e', 'c'],
+    ['c', 'f'],
+]
+testVertices.forEach(vertex => testGraph.addVertex(vertex))
+testEdges.forEach(edgeArray => testGraph.addEdge(edgeArray[0], edgeArray[1]))
+testGraph.printGraph()
--- secretWord.js
+++ secretWord.js
@@ -97,115 +97,177 @@ console.log(recoverSecret(triplets1))
 
 
 
+// function recoverSecret(tripArray) {
+//     let anOb = {}
+//     tripArray.forEach(triplet => {
+//         triplet.forEach((letter, index) => {
+//             if (!anOb[letter]) { anOb[letter] = {} }
+//             if (!anOb[letter][index]) { anOb[letter][index] = 1 } else { anOb[letter][index]++ }
+//         })
+//     })
+//     console.log(anOb)//now we have some analytics to begin our deduction
+//     // let beginning = ""
+//     // let end = ""
+//     let totalLetters = 0
+//     let unplacedLetters = []
+//     for (let letter in anOb) {
+//         unplacedLetters.push(letter)
+//         totalLetters++
+//         let letterCount = 0
+//         let value = 0
+//         for (let index in anOb[letter]) {
+//             letterCount += anOb[letter][index]
+//             index == '0' ? value -= anOb[letter][index] : index == "2" ? value += anOb[letter][index] : value += 0;
+//         }
+//         // if (!anOb[letter]["2"] && !anOb[letter]["1"]) {//this isn't rigorous
+//         //     console.log(`${letter} must be the beginning`)
+//         //     beginning = letter
+//         // }
+//         // if (!anOb[letter]["0"] && !anOb[letter]["1"]) {//this isn't rigorous, but a start
+//         //     console.log(`${letter} must be the end`)
+//         //     end = letter
+//         // }
+//         anOb[letter].solved = false
+//         anOb[letter].count = letterCount
+//         anOb[letter].value = value
+//         anOb[letter].comesBefore = []
+//         anOb[letter].comesAfter = []
+//     }
+//     tripArray.forEach(triplet => {
+//         let first = triplet[0]
+//         let second = triplet[1]
+//         let third = triplet[2]
+//         anOb[first].comesBefore.push(second, third)
+//         anOb[third].comesAfter.push(second, first)
+//         anOb[second].comesBefore.push(third)
+//         anOb[second].comesAfter.push(first)
+//     })
+//     for (let letter in anOb) {
+//         anOb[letter].comesBefore = new Array(... new Set(anOb[letter].comesBefore))
+//         anOb[letter].comesAfter = new Array(... new Set(anOb[letter].comesAfter))
+//         anOb[letter].comesBeforeCount = anOb[letter].comesBefore.length
+//         anOb[letter].comesAfterCount = anOb[letter].comesAfter.length
+//         anOb[letter].balance = anOb[letter].comesAfterCount - anOb[letter].comesBeforeCount
+//         anOb[letter].implications = anOb[letter].comesAfterCount + anOb[letter].comesBeforeCount
+//         anOb[letter].middleness = anOb[letter].implications - Math.abs(anOb[letter].balance)//a middleness of 2 (or 0) seems to be part of the mechanics of generating the minimum triplets that define a graph
+//     }
+//     console.log(anOb)//now we have hella analytics to make our deduction
+//     // console.log(beginning)//may have to go
+//     // console.log(end)//may have to go
+//     console.log(totalLetters)
+//     console.log(unplacedLetters)
+//     console.log("-----------------------")
+//     // find a triplet that doesn't contain beginning or end
+//     // let sortedArray = []
+//     // for (let i = 0; i < tripArray.length; i++) {//find a triplet that doesn't include beginning or end to seed our sorted array
+//     //     if (tripArray[i].indexOf(beginning) == -1 && tripArray[i].indexOf(end) == -1) {
+//     //         sortedArray = tripArray[i]
+//     //         break
+//     //     }
+//     // }
+//     let sortedArray = tripArray[0]
+//     sortedArray.forEach(letter => removeElement(unplacedLetters, letter))
+//     // sortedArray.push(end)
+//     // removeElement(unplacedLetters, end)
+//     // sortedArray.unshift(beginning)
+//     // removeElement(unplacedLetters, beginning)
+//     console.log(sortedArray, unplacedLetters)
+
+//     // go through the unplaced letters and fit them somewhere
+//     let counter = 0
+//     // while (unplacedLetters.length > 0) {//the real one
+//     // while (counter <= 10) {
+//     while (counter <= unplacedLetters.length) {
+//         let currentLetter = unplacedLetters[counter]
+//         let facts = anOb[currentLetter]
+//         console.log(currentLetter)
+//         //lets use the Array.some()methods
+//         for (let i = 0; i < sortedArray.length; i++) {
+
+//             let before = sortedArray.slice(0, i + 1)
+//             let after = sortedArray.slice(i)
+//             if (before.some(x => facts.comesBefore.includes(x))) {
+//                 console.log("it doesn't belong", before, facts.comesAfter)
+//             } else if (after.some(x => facts.comesAfter.includes(x))) {
+//                 console.log("it doesn't belong", after, facts.comesBefore)
+//             }
+//             else {
+//                 console.log("it does belong")
+//                 sortedArray.splice(i, 0, currentLetter)
+//                 removeElement(unplacedLetters, currentLetter)
+//                 break
+//             }
+//         }
+//         console.log(sortedArray, unplacedLetters, counter)
+//         counter += 1//testing
+//         // counter = (counter + 1) % unplacedLetters.length//the real one
+//     }
+//     return sortedArray.join("")
+
+// }
+// function removeElement(uniqueArray, elementToRemove) {
+//     let index = uniqueArray.indexOf(elementToRemove)
+//     uniqueArray.splice(index, 1)
+// }
+
+
+// funky fresher start on the problem as a Graph
+
+
+
 function recoverSecret(tripArray) {
-    let anOb = {}
+    class Graph {
+        constructor(noOfVertices) {
+            this.noOfVertices = noOfVertices
+            this.AdjList = new Map();
+        }
+        addVertex(v) {
+            this.AdjList.set(v, [])
+        }
+        addDirectedEdge(v, w) {// v= source, w=destingation
+            this.AdjList.get(v).push(w)
+        }
+        // addEdge(v, w) {//for undirected graph
+        //     this.AdjList.get(v).push(w)
+        //     this.AdjList.get(w).push(v)
+        // }
+        printGraph() {
+            let get_vertices = this.AdjList.keys()
+            for (let vertex of get_vertices) {
+                let values = this.AdjList.get(vertex)
+                let adjacencies = ""
+                for (let edge of values) {
+                    adjacencies += `${edge} `
+                }
+                console.log(`${vertex} -> ${adjacencies}`)
+            }
+        }
+        // bfs(startingNode)//breadth first search
+        // dfs(startingNode)//depth first search
+    }
+    let vertices = []
+    let edges = []
     tripArray.forEach(triplet => {
-        triplet.forEach((letter, index) => {
-            if (!anOb[letter]) { anOb[letter] = {} }
-            if (!anOb[letter][index]) { anOb[letter][index] = 1 } else { anOb[letter][index]++ }
+        triplet.forEach(letter => {
+            if (!vertices.includes(letter)) { vertices.push(letter) }
         })
+        if (!edges.includes(`${triplet[0]}->${triplet[1]}`)) { edges.push(`${triplet[0]}->${triplet[1]}`) }///this isn't working, perhaps includes doesn't like to search arrays
+        if (!edges.includes(`${triplet[0]}->${triplet[2]}`)) { edges.push(`${triplet[0]}->${triplet[2]}`) }
+        if (!edges.includes(`${triplet[1]}->${triplet[2]}`)) { edges.push(`${triplet[1]}->${triplet[2]}`) }
+        // edges.push([triplet[0], triplet[1]], [triplet[0], triplet[2]], [triplet[1], triplet[2]])
     })
-    console.log(anOb)//now we have some analytics to begin our deduction
-    // let beginning = ""
-    // let end = ""
-    let totalLetters = 0
-    let unplacedLetters = []
-    for (let letter in anOb) {
-        unplacedLetters.push(letter)
-        totalLetters++
-        let letterCount = 0
-        let value = 0
-        for (let index in anOb[letter]) {
-            letterCount += anOb[letter][index]
-            index == '0' ? value -= anOb[letter][index] : index == "2" ? value += anOb[letter][index] : value += 0;
-        }
-        // if (!anOb[letter]["2"] && !anOb[letter]["1"]) {//this isn't rigorous
-        //     console.log(`${letter} must be the beginning`)
-        //     beginning = letter
-        // }
-        // if (!anOb[letter]["0"] && !anOb[letter]["1"]) {//this isn't rigorous, but a start
-        //     console.log(`${letter} must be the end`)
-        //     end = letter
-        // }
-        anOb[letter].solved = false
-        anOb[letter].count = letterCount
-        anOb[letter].value = value
-        anOb[letter].comesBefore = []
-        anOb[letter].comesAfter = []
-    }
-    tripArray.forEach(triplet => {
-        let first = triplet[0]
-        let second = triplet[1]
-        let third = triplet[2]
-        anOb[first].comesBefore.push(second, third)
-        anOb[third].comesAfter.push(second, first)
-        anOb[second].comesBefore.push(third)
-        anOb[second].comesAfter.push(first)
+    let secretGraph = new Graph(vertices.length)
+    vertices.forEach(vertex => secretGraph.addVertex(vertex))
+    // let cleanEdges = new Array(...new Set(edges))
+    edges.forEach(edge => {
+        let local = edge.split('->')
+        secretGraph.addDirectedEdge(local[0], local[1])
     })
-    for (let letter in anOb) {
-        anOb[letter].comesBefore = new Array(... new Set(anOb[letter].comesBefore))
-        anOb[letter].comesAfter = new Array(... new Set(anOb[letter].comesAfter))
-        anOb[letter].comesBeforeCount = anOb[letter].comesBefore.length
-        anOb[letter].comesAfterCount = anOb[letter].comesAfter.length
-        anOb[letter].balance = anOb[letter].comesAfterCount - anOb[letter].comesBeforeCount
-        anOb[letter].implications = anOb[letter].comesAfterCount + anOb[letter].comesBeforeCount
-        anOb[letter].middleness = anOb[letter].implications - Math.abs(anOb[letter].balance)//a middleness of 2 (or 0) seems to be part of the mechanics of generating the minimum triplets that define a graph
-    }
-    console.log(anOb)//now we have hella analytics to make our deduction
-    // console.log(beginning)//may have to go
-    // console.log(end)//may have to go
-    console.log(totalLetters)
-    console.log(unplacedLetters)
-    console.log("-----------------------")
-    // find a triplet that doesn't contain beginning or end
-    // let sortedArray = []
-    // for (let i = 0; i < tripArray.length; i++) {//find a triplet that doesn't include beginning or end to seed our sorted array
-    //     if (tripArray[i].indexOf(beginning) == -1 && tripArray[i].indexOf(end) == -1) {
-    //         sortedArray = tripArray[i]
-    //         break
-    //     }
-    // }
-    let sortedArray = tripArray[0]
-    sortedArray.forEach(letter => removeElement(unplacedLetters, letter))
-    // sortedArray.push(end)
-    // removeElement(unplacedLetters, end)
-    // sortedArray.unshift(beginning)
-    // removeElement(unplacedLetters, beginning)
-    console.log(sortedArray, unplacedLetters)
-
-    // go through the unplaced letters and fit them somewhere
-    let counter = 0
-    // while (unplacedLetters.length > 0) {//the real one
-    // while (counter <= 10) {
-    while (counter <= unplacedLetters.length) {
-        let currentLetter = unplacedLetters[counter]
-        let facts = anOb[currentLetter]
-        console.log(currentLetter)
-        //lets use the Array.some()methods
-        for (let i = 0; i < sortedArray.length; i++) {
-
-            let before = sortedArray.slice(0, i + 1)
-            let after = sortedArray.slice(i)
-            if (before.some(x => facts.comesBefore.includes(x))) {
-                console.log("it doesn't belong", before, facts.comesAfter)
-            } else if (after.some(x => facts.comesAfter.includes(x))) {
-                console.log("it doesn't belong", after, facts.comesBefore)
-            }
-            else {
-                console.log("it does belong")
-                sortedArray.splice(i, 0, currentLetter)
-                removeElement(unplacedLetters, currentLetter)
-                break
-            }
-        }
-        console.log(sortedArray, unplacedLetters, counter)
-        counter += 1//testing
-        // counter = (counter + 1) % unplacedLetters.length//the real one
-    }
-    return sortedArray.join("")
-
-}
-function removeElement(uniqueArray, elementToRemove) {
-    let index = uniqueArray.indexOf(elementToRemove)
-    uniqueArray.splice(index, 1)
+    secretGraph.printGraph()
+    console.log("a", vertices)
+    console.log("b", edges)
+    // console.log("c", cleanEdges)
+    console.log(vertices.length)
+    console.log(edges.length)
+    // console.log(cleanEdges.length)
 }
