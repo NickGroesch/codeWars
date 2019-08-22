3b91c35a025de4507acdd03c5d8c8cac002bb8ca
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-08-22 13:31:54 -0500

    working on understanding directed graphs via adjecency list

---
+++ directedGraph.js
@@ -0,0 +1,11 @@
+class Graph {
+    constructor(noOfVertices) {
+        this.noOfVertices = noOfVertices
+        this.AdjList = new Map();
+    }
+    //addVertex (v),
+    // addEdge(v,w)
+    // printGraph()
+    // bfs(v)
+    // dfs(v)
+}
