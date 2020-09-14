39c31fd47e831264e6d0a2ecda3b91ade79eeedf
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2020-09-14 11:11:20 -0500

    begin work on parseIntReloaded, and study of grammers

---
+++ parseIntReloaded.js
@@ -0,0 +1,7 @@
+function parseInt(string) {
+    // Lex: tokenize a stream of text
+    //   chop into discrete tokens
+    let tokens = string.split(" ")
+    //   identify token type (not)
+    //   output a stream of tokens
+}
